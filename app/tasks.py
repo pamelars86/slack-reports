cab502b9373450b1b619f2b94fcad8fb7ef8e341
Pamela Rosales <pamela.rosales@pucp.pe>
2024-11-15 15:09:01 -0300

    fix to add user name

--- app/tasks.py
+++ app/tasks.py
@@ -2,7 +2,16 @@ from . import celery
 from .slack_client import fetch_messages
 from collections import defaultdict
 from datetime import datetime, timedelta
+from . import logger
+from .utils import get_user_info_by_user_id
+from slack_sdk import WebClient
+import os
 
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
+
+client = WebClient(token=os.getenv("SLACK_TOKEN"))
 
 @celery.task(bind=True)
 def fetch_messages_task(self, channel_id, p_start_date, p_end_date):
@@ -28,12 +37,9 @@ def fetch_messages_task(self, channel_id, p_start_date, p_end_date):
 
     return messages
 
-
-
-
 @celery.task(bind=True)
 def calculate_top_repliers_task(self, channel_id, p_start_date, p_end_date, top_n=10):
-    top_repliers_data = defaultdict(lambda: {"discussions": 0, "responses": 0})
+    top_repliers_data = defaultdict(lambda: {"discussions": 0, "responses": 0, "full_name": ""})
     
     chunk_size = timedelta(days=7)  # Ex.: process 7 days at a time
 
@@ -49,9 +55,8 @@ def calculate_top_repliers_task(self, channel_id, p_start_date, p_end_date, top_
         
         for msg in messages:
             parent_author = msg['author']
-            discussion_id = msg['post_id']
             repliers_set = set()
-            
+
             for reply in msg.get("replies", []):
                 reply_author = reply["author"]
 
@@ -60,11 +65,29 @@ def calculate_top_repliers_task(self, channel_id, p_start_date, p_end_date, top_
                         top_repliers_data[reply_author]["discussions"] += 1
                         repliers_set.add(reply_author)
                     top_repliers_data[reply_author]["responses"] += 1
-        
+
         current_start = current_end
 
     sorted_repliers = sorted(
-        top_repliers_data.items(), 
+        top_repliers_data.items(),
         key=lambda x: (-x[1]['discussions'], -x[1]['responses'])
-    )
-    return sorted_repliers[:top_n]
+    )[:top_n]
+
+    top_repliers_ids = [author for author, _ in sorted_repliers]
+
+    full_names = {
+        user_id: get_user_info_by_user_id(client, user_id)
+        for user_id in top_repliers_ids
+    }
+
+    result = [
+        {
+            "id_replier": author,
+            "full_name_replier": full_names.get(author, "Unknown"),
+            "discussions": data["discussions"],
+            "responses": data["responses"]
+        }
+        for author, data in sorted_repliers
+    ]
+
+    return result
